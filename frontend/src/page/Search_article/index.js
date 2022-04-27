3f54eabada5b6a7325379a484f95c5c7de2197e3
Jason-5-37 <2207324024@qq.com>
2022-04-27 12:04:37 +1200

    set up the router & finish the 404

---
+++ frontend/src/page/Search_article/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Search_article extends Component {
+
+  render() {
+    return (
+        <h1>Search_article</h1>
+    )
+  }
+}
+
+export default Search_article;
