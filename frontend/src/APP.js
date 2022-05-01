f85ee88f9e7f9aa731e5260d61d84cd3266d4da3
ketchup3313 <skw0852@autuni.ac.nz>
2022-05-01 16:34:33 +0800

    update package.json

--- frontend/src/APP.js
+++ frontend/src/APP.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route,Link,Switch} from 'react-router-dom';
 // import Link from 'umi/link';
 import Contactus from './Component/Contactus';
 import Home from './Component/Home';
+import Submit from './Component/Submit';
 import {
   PieChartOutlined,
   DesktopOutlined 
@@ -35,26 +36,25 @@ class SiderDemo extends Component {
               >
                 <Menu.Item key="/home" >
                <Link to="/home"> <PieChartOutlined />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Article</Link>
-                
                 </Menu.Item>
-                {/* <Menu.Item key="/home/list" icon={<DiffOutlined />}>
-                  <Link to="/home/list">内容管理</Link>
-                </Menu.Item> */}
-                <Menu.Item key="/contactus">
-                  <Link to="/contactus"><DesktopOutlined />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact us</Link>
+          
+                <Menu.Item key="/home/contactus">
+                  <Link to="/home/contactus"><DesktopOutlined />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact us</Link>
                 </Menu.Item>
+
+                <Menu.Item key="/home/submit">
+                  <Link to="/home/submit"><DesktopOutlined />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Submit</Link></Menu.Item>
               </Menu>
+
+              
         </Sider>
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
             <Switch>
                   <Route exact path="/home" component={Home}></Route>
-                  <Route path="/contactus" component={Contactus}></Route>
+                  <Route path="/home/contactus" component={Contactus}></Route>
+                  <Route path="/home/submit" component={Submit}></Route>
                 </Switch>
           </Content>
         
