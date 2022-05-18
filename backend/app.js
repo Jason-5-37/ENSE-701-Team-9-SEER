fdb849a7ccd5bd3753ad4e2552cc00c02558c68b
Jason-5-37 <2207324024@qq.com>
2022-05-18 18:01:11 +1200

    commit message

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,17 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+
 app.get('/', (req, res) => res.send('Welcome to SPEED!'));
 
 // use Routes
