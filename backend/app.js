cc4537c7c0e896b7c86083af2e60a864529b36bd
scottatlas <scogranger@gmail.com>
2022-05-18 19:38:26 +1200

    back end

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,43 @@
+// app.js
+
 const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const articles = require('./routes/api/articles');
+const NotSubmitArticles = require('./routes/api/NotSubmitArticles');
+const Manager = require('./routes/api/managers');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Running")
-})
+// Connect Database
+connectDB();
 
-Server.listen(5000, console.log("Server is working on 5000"));
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+
+app.get('/', (req, res) => res.send('Welcome to SPEED!'));
+
+// use Routes
+app.use('/api/articles', articles);
+app.use('/api/NotSubmitArticles', NotSubmitArticles);
+app.use('/api/manager', Manager);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ backend/routes/api/managers.js
@@ -0,0 +1,69 @@
+// routes/api/books.js
+
+const express = require('express');
+const router = express.Router();
+
+// Load Book model
+const Manager = require('../../models/manager');
+
+// @route GET api/books/test
+// @description tests books route
+// @access Public
+router.get('/test', (req, res) => res.send('Article route testing!'));
+
+// @route GET api/books
+// @description Get all books
+// @access Public
+router.get('/', (req, res) => {
+    Manager.find()
+    .then(managers => res.json(managers))
+    .catch(err => res.status(404).json({ nobooksfound: 'No article found' }));
+});
+
+// @route GET api/books/:id
+// @description Get single book by id
+// @access Public
+
+router.get('/:id', (req, res) => {
+    Manager.findById(req.params.id)
+    .then(manager => res.json(manager))
+    .catch(err => res.status(404).json({ nobookfound: 'No article found' }));
+});
+
+
+router.post('/Search_manager', (req, res) => {
+    Manager.find({'email':req.body.email})
+    .then(manager => res.json(manager))
+    .catch(err => res.status(404).json({ nobookfound: 'asd' }));
+});
+
+// @route GET api/books
+// @description add/save book
+// @access Public
+router.post('/', (req, res) => {
+    Manager.create(req.body)
+    .then(manager => res.json({ msg: 'article added successfully' }))
+    .catch(err => res.status(400).json({ error: 'Unable to add this article' }));
+});
+
+// @route GET api/books/:id
+// @description Update book
+// @access Public
+router.post('/:id', (req, res) => {
+    Manager.findByIdAndUpdate(req.params.id, req.body)
+    .then(manager => res.json({ msg: 'Updated successfully' }))
+    .catch(err =>
+      res.status(400).json({ error: 'Unable to update the Database' })
+    );
+});
+
+// @route GET api/books/:id
+// @description Delete book by id
+// @access Public
+router.delete('/:id', (req, res) => {
+    Manager.findByIdAndRemove(req.params.id, req.body)
+    .then(manager => res.json({ mgs: 'article entry deleted successfully' }))
+    .catch(err => res.status(404).json({ error: 'No such an article' }));
+});
+
+module.exports = router;
