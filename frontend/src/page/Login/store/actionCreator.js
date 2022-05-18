1efdac6c271d1a55c94f90272bca2e6f079fade4
scottatlas <scogranger@gmail.com>
2022-05-18 19:37:23 +1200

    login in page

---
+++ frontend/src/page/Login/store/actionCreator.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { fromJS } from 'immutable';
+
+export const login = (Email, Password) =>{
+    return() =>{
+        let postdate = {
+            "email": Email,
+            "password": Password
+        }
+        axios.post('http://localhost:8082/api/manager/Search_manager',postdate).then((res) =>{
+            const data = res.data;
+            //console.log(data[0].email);
+            if(data[0].email === Email && data[0].password === Password){
+                var storage=window.localStorage;
+                storage.setItem("Islogin", "login");
+                //console.log("login");
+                window.location.href = './';
+            }
+        }).catch(() => {
+            console.log('error: Can not get Search List');
+        })
+    }
+}
---
+++ frontend/src/page/Login/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const LoginWrapper = styled.div`
+    width:100 ;
+    color:White;
+    text-align: center;
+    margin-top:15px;
+    margin-bottom:10px;
+    `;
+
+export const LoginTitle = styled.div`
+    height: 20%;
+    width: 100%;
+    background-color:#161B22;
+    color:White;
+    margin: 5% auto;
+    font-size: 200%;
+    text-align: center;
+`;
+
+export const Content = styled.div`
+    margin:1% auto;
+`
